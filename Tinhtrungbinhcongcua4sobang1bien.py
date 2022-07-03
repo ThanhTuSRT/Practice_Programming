7e8b6b569dc32abb194b6f5cf0502c90a24c620d
ThanhTuSRT <108538480+ThanhTuSRT@users.noreply.github.com>
2022-07-03 17:02:16 +0700

    File

---
+++ Tinhtrungbinhcongcua4sobang1bien.py
@@ -0,0 +1,11 @@
+S = 0
+n=int(input("Nhập số thứ nhất: "))
+S = S + n
+n=int(input("Nhập số thứ hai: "))
+S = S + n
+n=int(input("Nhập số thứ ba: "))
+S = S + n
+n=int(input("Nhập số thứ tư: "))
+S = S + n
+
+print('Trung bình cộng của bốn số đó là ', S/4)
